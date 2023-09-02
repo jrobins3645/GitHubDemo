119c1fb7799b80f2fc88dad8bd4eee42bca8e228
jrobins3645 <jeff.h.robinson@gmail.com>
2023-09-01 20:50:48 -0400

    Initial Commit

---
+++ GitHubDemo/GitHubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemoApp.swift
+//  GitHubDemo
+//
+//  Created by Jeff on 9/1/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
